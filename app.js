2dce4d36e89d2cd92ffa9f3cd8d46e843cfbd870
LordNinth <rr621801@gmail.com>
2024-03-30 22:27:08 +0000

    cookiesParser

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser"); //cookie parser package
+
+//loading cookieParser in express to parse cookies
+app.use(cookieParser("thisissecret")); //adding secret msg in cookieParser to support signed cookie
+
+app.get("/greet", (req, res) => {
+  // console.log(req.cookies)
+  const { name = "noName" } = req.cookies;
+  res.send(`hey there ${name}`);
+});
+
+app.get("/setname", (req, res) => {
+  res.cookie("name", "xhinsoo");
+  res.cookie("message", "You suck");
+  res.send("Setting cookies");
+});
+
+app.get("/getsignedcookies", (req, res) => {
+  res.cookie("fruit", "grape", { signed: true });
+  res.send("signed cookies");
+});
+
+app.get("/getverified", (req, res) => {
+  res.send(req.cookies); //cannot see signed cookies
+  res.send(req.signedCookies); //can see signed cookies
+});
+app.listen(3000, () => {
+  console.log("listening 3000");
+});
---
+++ note.md
@@ -0,0 +1,2 @@
+From express app, we can respond with cookies. Browser saves these cookies.
+we can send: domain name, expiry date, maxAge, path "/", signed and so on.
